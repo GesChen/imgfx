cfd3f5d20995b4095d988faebd9db240d027f054
GesChen <omgtntlol101@gmail.com>
2023-08-19 21:53:40 -0400

    many things
    threading, average fix, moved outline to readme, fixed some formatting, implemented grouping using multithreading

--- blocks/blocks.py
+++ blocks/blocks.py
@@ -4,121 +4,137 @@ import random
 from matplotlib import pyplot as plt
 import datetime
 import os
-
-'''
-performance function factors
-	+closeness in pixels to actual image
-	-number of blocks
-iteration:
-	multiple "agents" are made, each make some number of changes
-	grade performance
-	base next generation off best performing
-
-image structure:
-	list of block dicts
-	{bottom left, top right, color}
-'''
+import threading
 
 # get the average color over a region of an image
 def region_average(image, pos0, pos1):
-	return np.mean(image[
-		min(pos0[0], pos1[0]):max(pos0[0],pos1[0]), 
-		min(pos0[1], pos1[1]):max(pos0[1],pos1[1])])
+    region = image[
+        min(pos0[0], pos1[0]):max(pos0[0], pos1[0]), 
+        min(pos0[1], pos1[1]):max(pos0[1], pos1[1])]
+    return np.mean(region, axis=(0,1))
 
 # use image structure to generate image using rectangles 
 def generate_image(image, original):
-	blank = np.zeros([image[0]['size'][0],image[0]['size'][1],3], dtype=np.uint8)
-	if len(image) == 1:
-		return blank
-	
-	for block in image[1:]:
-		cv2.rectangle(blank, block['pos0'], block['pos1'], region_average(original, block['pos0'], block['pos1']), -1)
-	return blank
+    blank = np.zeros([image[0]['size'][1], image[0]['size'][0], 3], dtype=np.uint8)
+    if len(image) == 1:
+        return blank
+    
+    for block in image[1:]:
+        cv2.rectangle(blank, block['pos0'], block['pos1'], region_average(original, block['pos0'], block['pos1']), -1)
+    return blank
 
 # absolute element wise difference of colors
 def difference(col1, col2):
-	return np.sum(np.abs(col1-col2)) 
+    return np.sum(np.abs(col1 - col2)) 
 
 # calculate total pixel difference multiplied by number of blocks 
 def performance(original, image): 
-	actual = generate_image(image, original)
-	total = 0
-	for row in range(actual.shape[0]):
-		for column in range(actual.shape[1]):
-			total += difference(actual[row][column], original[row][column])
-	return total# * (len(image) - 1)
+    actual = generate_image(image, original)
+    total = 0
+    for row in range(actual.shape[0]):
+        for column in range(actual.shape[1]):
+            total += difference(actual[row][column], original[row][column])
+    return total# * (len(image) - 1)
 
 # perform a random change to the image
 def random_change(image):
-	choice = random.randrange(0, 3) # pick a random choice
-	if len(image) == 1: # force add a rect if empty image
-		choice = 0
-	match choice:
-		case 0: # add a random rect
-			image.append({
-				'pos0'  : (int(random.randrange(0, image[0]['size'][1])), int(random.randrange(0, image[0]['size'][0]))),
-				'pos1'  : (int(random.randrange(0, image[0]['size'][1])), int(random.randrange(0, image[0]['size'][0]))),
-			})
-		case 1: # remove a random rect
-			del image[random.randrange(1, len(image))]
-		
-		case 2: # move a random rect
-			image[random.randrange(1, len(image))]['pos0'] = (int(random.randrange(0, image[0]['size'][0])), int(random.randrange(0, image[0]['size'][1])))
-			image[random.randrange(1, len(image))]['pos1']   = (int(random.randrange(0, image[0]['size'][0])), int(random.randrange(0, image[0]['size'][1])))
-	return image 
+    choice = random.randrange(0, 3) # pick a random choice
+    if len(image) == 1: # force add a rect if empty image
+        choice = 0
+    match choice:
+        case 0: # add a random rect
+            image.append({
+                'pos0'  : (int(random.randrange(0, image[0]['size'][0])), int(random.randrange(0, image[0]['size'][1]))),
+                'pos1'  : (int(random.randrange(0, image[0]['size'][0])), int(random.randrange(0, image[0]['size'][1]))),
+            })
+        case 1: # remove a random rect
+            del image[random.randrange(1, len(image))]
+        
+        case 2: # move a random rect
+            image[random.randrange(1, len(image))]['pos0'] = (int(random.randrange(0, image[0]['size'][1])), int(random.randrange(0, image[0]['size'][0])))
+            image[random.randrange(1, len(image))]['pos1'] = (int(random.randrange(0, image[0]['size'][1])), int(random.randrange(0, image[0]['size'][0])))
+    return image 
 
+# performs random number of random changes to an 
+# image and sets the resulting performance and image in results
 def mutate(image, maxchanges):
-	for c in range(random.randint(1, maxchanges)):
-		image = random_change(image)
-	perf = performance(original, image)
-	return perf, image
+    for c in range(maxchanges):
+        image = random_change(image)
+    perf = performance(original, image)
+    return perf, image
+
+# performs batch number of mutations as a group
+def group(image, maxchanges, batch_size, output, group_index):
+    best_performance = np.inf
+    best_image = None
+    for i in range(batch_size):
+        #print(f"processing item {i} in group {group_index}", flush=True)
+        perf, thisImage = mutate(image[::], maxchanges)
+        if perf < best_performance:
+            best_performance = perf
+            best_image = thisImage
+    output[group_index] = [best_performance, best_image]
+
 
 # init
 path = r"D:\Projects\Programming\Python Scripts\.image effects\imgfx\blocks\output"
 file = r"D:\Projects\Programming\Python Scripts\.image effects\imgfx\blocks\candle_downscale.png"
 original = cv2.cvtColor(
-	cv2.imread(file),
-	cv2.COLOR_BGR2RGB)
+    cv2.imread(file),
+    cv2.COLOR_BGR2RGB)
 
 iterations = 10
-batch_size = 5
-changes = 30
-batch = [[{'size' : original.shape[:2]}]] * batch_size # create a bunch of image 
+groups = 30
+batch_size = 10
+changes = 50
+closest_image = [{'size' : original.shape[:2][::-1]}] # create a blank image 
 
 perf_history = []
-result = None
+final = None
 last_lowest = np.Infinity
 # main loop
 for i in range(iterations):
-	closest_image = batch[0]
-	lowest_performance = last_lowest
-	for image in batch[1:]:
-		perf, image = mutate(image, changes)
-		
-		if perf < lowest_performance:
-			lowest_performance = perf
-			closest_image = image		
-	
-	batch = [closest_image] * batch_size	
+    threads = []
+    results = [[]] * groups
+    for t in range(groups):
+        print(f"processing group {t}")
+        thread = threading.Thread(target = group, args = (closest_image, changes, batch_size, results, t))
+        threads.append(thread)
+        thread.name = "Group " + str(t)
+        thread.start()
+    for t in threads:
+        t.join()
+    
+    lowest_performance = last_lowest
+    for g, result in enumerate(results):
+        perf, image = result
+        print(f"group index {g} had a best performance of {perf}")
+        if perf < lowest_performance:
+            lowest_performance = perf
+            closest_image = image
+    
+    if lowest_performance == last_lowest:
+        print(f"iteration {i} had no improvements, best perf {lowest_performance}")
+    else:
+        print(f"iteration {i} best perf {lowest_performance}")
+    
+    last_lowest = lowest_performance
+    perf_history.append(lowest_performance)
+    final = closest_image
+    i+=1
 
-	if lowest_performance == last_lowest:
-		print(f"iteration {i} had no improvements, best perf {lowest_performance}")
-	else:
-		print(f"iteration {i} best perf {lowest_performance}")
-	
-	last_lowest = lowest_performance
-	perf_history.append(lowest_performance)
-	result = closest_image
-	i+=1
-
-final = generate_image(result, original)
+final_image = generate_image(final, original)
 
 # find the highest outputted file name and add 1 
 output_files = [filename for filename in os.listdir(path) if filename.startswith('output')]
 highest_number = max([int(filename[6:-4]) for filename in output_files])
 
 filename = f"output{highest_number + 1}.png"
-cv2.imwrite(os.path.join(path, filename), cv2.cvtColor(final, cv2.COLOR_RGB2BGR))
+cv2.imwrite(os.path.join(path, filename), cv2.cvtColor(final_image, cv2.COLOR_RGB2BGR))
 
-plt.imshow(final)
+x_axis = range(iterations)
+plt.plot(x_axis, perf_history)
+plt.title('best performance by iteration')
+plt.xlabel('iteration')
+plt.ylabel('best performance')
 plt.show()
--- blocks/readme.md
+++ blocks/readme.md
@@ -2,3 +2,22 @@
 Uses "Machine Learning" (not sure what this even is) to recreate an image using rectangles of different sizes 
 Supposed to get better over time by "training"
 In the end you *should* get a relatively good image that represents the original to *some* degree of accuracy, so far though still needs lots of work.
+
+### Algo layout/psuedo:
+performance function factors
+    +closeness in pixels to actual image
+    -number of blocks
+
+image structure:
+    list of block dicts
+    {bottom left, top right, color}
+
+iteration:
+    2d results list 
+    groups are made on separate threads
+    per group: 
+        multiple "agents" are made, each make some number of changes (batch)
+        grade performance
+        return best performing
+    compare best performing to get bestest performing 
+    base next generation off bestest performing
